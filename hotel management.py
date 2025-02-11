8acb56344fd909c9dbef5d82df98a81a697a53c7
Niranjan ghising <ghisingniranjan@gmail.com>
2025-02-11 21:45:04 +0545

    Update TXT files

---
+++ hotel management.py
@@ -0,0 +1,19 @@
+"""
+Restaurant Management System
+APU Degree Program - Group Assignment
+"""
+import os
+from datetime import datetime
+
+# Symbolic Constants
+USER_FILES = {
+    'admin': 'admin.txt',
+    'manager': 'manager.txt',
+    'chef': 'chef.txt',
+    'customer': 'customer.txt'
+}
+MENU_FILE = 'menu.txt'
+ORDERS_FILE = 'orders.txt'
+FEEDBACK_FILE = 'feedback.txt'
+INGREDIENTS_FILE = 'ingredients.txt'
+MAX_LOGIN_ATTEMPTS = 3
